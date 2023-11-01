481fcaf1ca7d8a70cadebd5e432207b85d5b21d1
Luan Alves <luanalves.dev@gmail.com>
2023-11-01 10:43:35 -0300

    conteudo basico de python

--- ola_mundo.py
+++ ola_mundo.py
@@ -23,11 +23,51 @@
 # print("Sua idade é:", valor, "'valor' é do tipo:", type(valor))
 
 
-valor = 0 
+# valor = 0 
 
-try:
-    valor = int(input("Digite a sua idade: "))
-except ValueError as error:
-		print("não foi possível ler a sua idade, use somente números:", error)
-else: 
-	print("Sua idade é:", valor, "anos")
+# try:
+#     valor = int(input("Digite a sua idade: "))
+# except ValueError as error:
+# 		print("não foi possível ler a sua idade, use somente números:", error)
+# else: 
+# 	print("Sua idade é:", valor, "anos")
+
+#  for
+# for letra in "Iteris":
+# 		print(letra)
+
+# # While
+# valor = input("Digite 'banana': ")
+
+# while valor != "banana":
+# 	valor = input("Digite 'banana': ")
+# # else pode ser usado quando a condição não atende mais o while
+# else:
+# 	print("voce digitou:", valor)
+
+
+# if/else/elif 
+
+# valor = int(input("digite um número entre 0 e 9: "))
+
+# if 0 <= valor and valor < 5:
+# 	print("valor entre 0 e 4")
+# elif 5 <= valor and valor < 10:
+# 	print("valor entre 5 e 9")
+# else:
+# 	print("valor fora do intervalo")
+
+valor = int(input("Escolha uma das opções. De 1 a 3: "))
+mensagem = ""
+
+match valor:
+	case 1:
+		mensagem = "opção 1 escolhida"
+	case 2:
+		mensagem = "opção 2 escolhida"
+	case 3:
+		mensagem = "opção 3 escolhida"
+	# default statement
+	case _:
+		mensagem = "opção inválida"
+print(mensagem)
